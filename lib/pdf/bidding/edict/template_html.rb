9b77c6f188d33e7f04b82d0167e7bf441a916832
Pedro Ivo <ivopedrobr@gmail.com>
2020-10-29 11:46:24 -0300

    number_to_currency

--- lib/pdf/bidding/edict/template_html.rb
+++ lib/pdf/bidding/edict/template_html.rb
@@ -104,7 +104,7 @@ module Pdf::Bidding
     end
 
     def format_money(money)
-      ActionController::Base.helpers.number_to_currency(money).to_s
+      number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
     end
 
     def surrond_with_table_tag
